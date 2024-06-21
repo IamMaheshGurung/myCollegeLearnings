c20bece882e28709995731ec62ab52b06d861a99
Mahesh <gurungmahesh167@gmail.com>
2024-06-22 08:12:47 +0900

    Practice

--- CS50/weekTwo/practice.c
+++ CS50/weekTwo/practice.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    char name[100]; // Buffer to store the input string
-
-    printf("Enter your name: ");
-    scanf("%99s", name); // Read up to 99 characters to prevent buffer overflow
-
-    printf("Your name is: %s\n", name);
+    const int N = 3;
+    //prototype
+    float average(int length, int array[]);
+    int main(){
+        int socre[N];
+        for (int i =0; i<N; i++){
+            scores[i] = get
+    
+    float average(int length, int array[])
+    {
+        // calculate average
+        int sum = 0;
+        for(int i =0; i<length; i++){
+            {
+                sum+=array[i];
+            }
+            return sum / (float) length;
 
     return 0;
 }
