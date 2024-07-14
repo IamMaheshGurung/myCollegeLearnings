b4e1c13a32c1119da3905b049604b4f4c25a4c42
Mahesh <gurungmahesh167@gmail.com>
2024-07-15 06:38:01 +0900

    Building Tictactoe game with basics

--- Manipal_Uni/file.c
+++ Manipal_Uni/file.c
@@ -3,16 +3,25 @@
 
 int main()
 {
+    //char fileName[100];
+    //printf("Which File Do You wanna Open?\n Type its Name correctly..");
+    //scanf("%s", fileName);
 
+    char buffer[255];
+    FILE *pFile = fopen("//Users//srijanamahesh//Desktop//myIncome.tsv", "r");
 
-
-
-
-
-
-    FILE *pFile = fopen("myfile.txt", "w");
-    fprintf(pFile, "HI Thank you for Looking this file\n");
-    fclose(pFile);
+    if(pFile != NULL) 
+    
+    {
+        while(fgets(buffer, 255, pFile)!=NULL)
+        {
+            printf("%s", buffer);
+        }
+    }
+    else
+    {
+        printf("Sorry File not found");
+    }
 
     return 0; 
 }
